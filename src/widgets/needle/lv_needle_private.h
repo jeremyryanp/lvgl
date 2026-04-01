42209611cd65cfd0fc56b98546ea1dd0488cc9a9
Jeremy <jeremyryanp@gmail.com>
2026-04-01 13:17:25 -0400

    missed

--- src/widgets/needle/lv_needle_private.h
+++ src/widgets/needle/lv_needle_private.h
@@ -34,15 +34,19 @@ struct _lv_needle_t {
     lv_value_precise_t pivot_x;
     lv_value_precise_t pivot_y;
 
-    lv_value_precise_t start_angle;  // Starting angle in degrees
-    lv_value_precise_t end_angle;    // Ending angle in degrees
-    lv_value_precise_t value;        // Current value (0-100)
-    lv_value_precise_t length;       // Length of the needle
-    lv_value_precise_t back_length;  // Length of the back part of the needle
+    lv_value_precise_t min_value;    // Logical value mapped to start_angle.
+    lv_value_precise_t max_value;    // Logical value mapped to end_angle.
+    lv_value_precise_t start_angle;  // Starting angle in degrees.
+    lv_value_precise_t end_angle;    // Ending angle in degrees.
+    lv_value_precise_t value;        // Current value.
+    lv_value_precise_t length;       // Length of the needle.
+    lv_value_precise_t back_length;  // Length of the back part of the needle.
 
     bool is_segment;
     lv_value_precise_t end_x;
     lv_value_precise_t end_y;
+    lv_area_t cached_area;
+    bool cached_area_valid;
 };
 
 
@@ -54,7 +58,7 @@ struct _lv_needle_t {
  *      MACROS
  **********************/
 
-#endif /* LV_USE_LINE != 0 */
+#endif /* LV_USE_NEEDLE != 0 */
 
 #ifdef __cplusplus
 } /*extern "C"*/
